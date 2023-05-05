80dae4911efc35dc528b7be4903dc6944bc707ce
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-05-05 00:39:23 -0400

    First commit

---
+++ src/pages/music.js
@@ -0,0 +1,10 @@
+import Nav from '/./components/Nav'
+
+export default function Music () {
+    return (
+        <>
+        <Nav />
+        <h1>This is a music page</h1>
+        </>
+    )
+}
---
+++ src/pages/photography.js
@@ -0,0 +1,10 @@
+import Nav from '/./components/Nav'
+
+export default function Photography () {
+    return (
+        <>
+        <Nav />
+        <h1>This is a photography page</h1>
+        </>
+    )
+}
