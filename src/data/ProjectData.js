e38fb979e1ef453f776e1d3feac6c42ffc437df6
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-10-02 17:04:15 -0400

    first commit

---
+++ src/data/ProjectData.js
@@ -0,0 +1,24 @@
+const ProjectData = [
+  {
+    title: "Robbie Collins",
+    src: "https://res.cloudinary.com/dwbwyzsyp/image/upload/v1687457354/Portfolio/Screenshot_2023-06-22_140801_ryrk6k.png",
+    description:
+      "This is a personal website for the trumpet player Robbie Collins",
+    tools: "Next.js, Tailwind, React Framer Motion, Cloudinary, Vercel",
+  },
+  {
+    title: "LexTalk Tech",
+    src: "https://res.cloudinary.com/dwbwyzsyp/image/upload/v1696268052/lextalk-home_yf7qtz.png",
+    description:
+      "This is the website for the quarterly tech conference LexTalk Tech",
+    tools: "Next.js, Tailwind, AOS, Netlify",
+  },
+  {
+    title: "Portfolio",
+    src: "/",
+    description: "You're here right now! This is my portfolio site",
+    tools: "Astro 3, ",
+  },
+];
+
+export default ProjectData;
