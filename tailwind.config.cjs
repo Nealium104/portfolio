b2ee1190fe969c2f1dc33776db28c221c29f9d9d
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-10-07 16:07:16 -0400

    finalized nav bar and tried out some colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,9 @@ module.exports = {
         sans: ["Fira Sans"],
       },
       colors: {
-        primary: "#15616D",
+        // primary: "#15616D",
+        primary: "#92A8D1",
+        coral: "#FF6F61",
       },
     },
   },
