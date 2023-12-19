30bcce78c7f1acc1f2788fb02bf6ef9e63d866dd
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-12-19 10:15:54 -0500

    added an in progress section

---
+++ src/data/inProgProjectData.js
@@ -0,0 +1,22 @@
+const inProgProjectData = [
+  //   {
+  //     title: "Skunch",
+  //     src: "https://res.cloudinary.com/dwbwyzsyp/image/upload/v1687457354/Portfolio/Screenshot_2023-06-22_140801_ryrk6k.png",
+  //     description:
+  //       "This is a website for the band Skunch from here in Lexington, KY!",
+  //     tools:
+  //       "Astro, Tailwind, Cloudinary, Contentful, Netlify, SendGrid, Node.js",
+  //     link: "https://skun.ch/",
+  //     github: "https://github.com/Nealium104/skunch",
+  //   },
+  {
+    title: "Tyler Helms Trumpet",
+    src: "https://res.cloudinary.com/dwbwyzsyp/image/upload/v1701047511/Screenshot_2023-11-26_195231_afzins.png",
+    description: "A website for a talented trumpet player from Memphis!",
+    tools:
+      "Astro, Tailwind, Cloudinary, Contentful, Netlify, SendGrid, Node.js",
+    link: "https://helmstrumpet.netlify.app/",
+    github: "https://github.com/Nealium104/helmstrumpet",
+  },
+];
+export default inProgProjectData;
